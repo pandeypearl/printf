c7ba45051398f5570a9425608b093b67801d20b2
Pretty Pandey <prettypandeypearl@gmail.com>
2022-10-17 09:54:35 -0700

    added print file and test file

---
+++ print.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _print_a_char - Prints a char
+ * @args: A list of variadic arguments
+ * Return: The length of the character
+ */
+
+int _print_a_char(va_list args)
+{
+	_write(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * _print_a_string - Prints a string
+ * @args: A list of variadic arguments
+ * Return: The length of the string
+ */
+
+int _print_a_string(va_list args)
+{
+	char *arg = va_arg(args, char *);
+	int i = 0;
+
+	if (arg != NULL)
+	{
+		while (arg[i])
+		{
+			_write(arg[i]);
+			i++;
+		}
+		return (i);
+	}
+
+	_write('(');
+	_write('n');
+	_write('u');
+	_write('l');
+	_write('l');
+	_write(')');
+	return (6);
+}
---
+++ test.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - A main to tests %c and %s cases in _printf()
+ * vs. printf()
+ * Return: Always zero.
+ */
+
+int main(void)
+{
+	int a = 0, b = 0;
+
+	return (0);
+}
